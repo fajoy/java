0a5c3b1ffdf9f674d054d330e42e12ba3536b969
fajoy <wuminfajoy@gmail.com>
2013-04-16 17:12:20 +0800

    add ant sample

---
+++ 00AntSample/src/fajoy/HelloWorld.java
@@ -0,0 +1,7 @@
+package fajoy;
+public class HelloWorld {
+	public static void main(String[] args) {
+        System.out.println("HelloWorld.");
+        System.out.println("This is main meassage.");
+	}
+}
