101f69d5c67104fed68da88a23b00e33df2f0011
fajoy <wuminfajoy@gmail.com>
2013-04-17 17:48:43 +0800

    update

--- 40S3Sample/src/fajoy/S3Sample.java
+++ 40S3Sample/src/fajoy/S3Sample.java
@@ -42,10 +42,7 @@ public class S3Sample {
         
         String configFile = System.getProperties().getProperty("user.dir")+"/s3.properties";
         System.out.print(configFile);
-        
-         
         Properties properties = new Properties();
-        
         try {
             properties.load(new FileInputStream(configFile));
         } catch (FileNotFoundException ex) {
@@ -65,14 +62,18 @@ public class S3Sample {
         System.out.println(secretKey);
         System.out.println(endpoint);
         
+        String bucketName="sample.bucket";
         
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
         AmazonS3 s3 = new AmazonS3Client(credentials);
         s3.setEndpoint(endpoint);
         S3ClientOptions clientOptions =new S3ClientOptions();
         clientOptions.setPathStyleAccess(true);
         s3.setS3ClientOptions(clientOptions );
+        
+        System.out.println("Creating bucket " + bucketName + "\n");
+        s3.createBucket(bucketName);
+        
         List<Bucket> buckets = s3.listBuckets();
         for (Bucket bucket : buckets) {
                     System.out.println(bucket.getName() + "\t" +
@@ -80,12 +81,54 @@ public class S3Sample {
         }
         
         System.out.println("Listing objects");
-        ObjectListing objectListing = s3.listObjects("hadoop");
+        ObjectListing objectListing = s3.listObjects(bucketName);
         for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
             System.out.println(" - " + objectSummary.getKey() + "  " +
                                "(size = " + objectSummary.getSize() + ")");
         }
+        
+        
+        System.out.println("Uploading a new object to S3 from a file\n");
+        String key = "MyObjectKey";
+        s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+        
+        System.out.println("Downloading an object");
+        S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+        System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+        displayTextInputStream(object.getObjectContent());
+        
+        
+        s3.deleteObject(bucketName, key);
+        s3.deleteBucket(bucketName);
 
+        
     }
+    
+    private static File createSampleFile() throws IOException {
+        File file = File.createTempFile("aws-java-sdk-", ".txt");
+        file.deleteOnExit();
+
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.close();
+
+        return file;
+    }
+
+    
+    private static void displayTextInputStream(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) break;
+            System.out.println("    " + line);
+        }
+        System.out.println();
+    }
+
 }
 
