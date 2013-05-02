39e116e535400b62cab9e8a244d463e418ec24ae
fajoy <wuminfajoy@gmail.com>
2013-05-02 19:56:00 +0800

    update

--- 21HDFSSample/src/fajoy/HDFSListSample.java
+++ 21HDFSSample/src/fajoy/HDFSListSample.java
@@ -9,23 +9,29 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class HDFSListSample {
-	public static final SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	public static final SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd HH:mm" +
+			"");
 	
 	public static void main(String[] args) throws Exception {
 		FileSystem fs = FileSystem.get(new Configuration());
 		Path path=new Path("./");
+		System.out.format("list %s \n",path.toUri().getPath());		
 		HDFSAction ls =new HDFSAction() {
 			@Override
 			public void actionDir(FileSystem fs, Path path, FileStatus fileStatus) {
-				System.out.format("%s %10s\t%s\n",dateForm.format(new Date( fileStatus.getModificationTime())),fileStatus.getLen(),path);
+				System.out.format("%s %10s\t%s\n"
+						,dateForm.format(new Date( fileStatus.getModificationTime()))
+						,fileStatus.getLen()
+						,path.toUri().getPath());
 			};
 			
 			
 			@Override
-			
 			public void actionFile(FileSystem fs, Path path,FileStatus fileStatus) {
-
-				System.out.format("%s %10s\t%s\n",dateForm.format(new Date( fileStatus.getModificationTime())),fileStatus.getLen(),path);
+				System.out.format("%s %10s\t%s\n"
+						,dateForm.format(new Date( fileStatus.getModificationTime()))
+						,fileStatus.getLen()
+						,path.toUri().getPath());
 			}
 		};
 		ls.run(fs, path, true);
