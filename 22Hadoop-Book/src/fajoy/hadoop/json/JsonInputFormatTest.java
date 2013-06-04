5dbac964bd9c8838e88097aa3018f9e931cdd5ad
fajoy <wuminfajoy@gmail.com>
2013-06-04 10:40:45 +0800

    update

--- 22Hadoop-Book/src/fajoy/hadoop/json/JsonInputFormatTest.java
+++ 22Hadoop-Book/src/fajoy/hadoop/json/JsonInputFormatTest.java
@@ -3,8 +3,11 @@ package fajoy.hadoop.json;
 import java.io.IOException;
 
 import com.alexholmes.json.mapreduce.MultiLineJsonInputFormat;
+import com.manning.hip.ch3.json.JsonInputFormat;
+import com.manning.hip.ch3.json.JsonMapReduce;
+import com.manning.hip.ch3.json.JsonMapReduce.Map;
 
-
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 
@@ -15,47 +18,49 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JsonInputFormatTest extends Configured implements Tool {
-  private static final Logger LOG = LoggerFactory.getLogger(JsonInputFormatTest.class);
+public class JsonInputFormatTest {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(JsonInputFormatTest.class);
 
-  private JsonInputFormatTest() {}
+	private JsonInputFormatTest() {
+	}
 
-  public static class JsonMapper extends Mapper<Text, Text, Text, Text> {
-        @Override
-    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-            context.write(key, value);
-    }
-  }
+	public static class JsonMapper extends Mapper<Text, Text, Text, Text> {
+		@Override
+		protected void map(Text key, Text value, Context context)
+				throws IOException, InterruptedException {
+			context.write(key, value);
+		}
+	}
 
-  public int run(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.out.println("Usage: hadoop jar path/to/this.jar " + getClass() + " <input dir> <output dir>");
-      System.exit(1);
-    }
+	public static void main(String... args) throws Exception {
+		runJob(args[0], args[1]);
+	}
 
-    Job job = new Job(getConf());
-    job.setJobName("JsonInputFormatTest");
-    job.setJarByClass(getClass());
-    
-    job.setInputFormatClass(MultiLineJsonInputFormat.class);
-    job.setMapperClass(JsonMapper.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
+	public static void runJob(String input, String output) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("multilinejsoninputformat.member", "results");
+		Job job = new Job(conf);
 
-    FileInputFormat.setInputPaths(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setJobName("JsonInputFormatTest");
+		job.setJarByClass(JsonInputFormatTest.class);
 
-    return job.waitForCompletion(true) ? 0 : 1;
-  }
+		job.setInputFormatClass(MultiLineJsonInputFormat.class);
+		job.setMapperClass(JsonMapper.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-  public static void main(String[] args) throws Exception {
-    int exitCode = ToolRunner.run(new JsonInputFormatTest(), args);
-    System.exit(exitCode);
-  }
+		FileInputFormat.setInputPaths(job, new Path(input));
+		Path outPath = new Path(output);
+		FileOutputFormat.setOutputPath(job, outPath);
+		outPath.getFileSystem(conf).delete(outPath, true);
+
+		job.waitForCompletion(true);
+	}
+
 }
